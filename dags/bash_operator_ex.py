f34e9f0731c995ae23f3ba5c1c554fa2ed8e5810
JSPARK__\jspar <jspark9703@gmail.com>
2024-08-25 15:07:05 +0900

    first commit

---
+++ dags/bash_operator_ex.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="bash_operator_ex",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 8, 26, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    # [END howto_operator_bash]
+    # [START howto_operator_bash]
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    # [END howto_operator_bash]
+
+    bash_t1 >> bash_t2
