3cff3a3b79612e2891de6228032fb125a9a11e00
root <root@jspark>
2024-08-26 17:52:28 +0900

    plugins 폴더 추가

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,8 @@
+FRUIT=$1
+if ["$FRUIT" == "APPLE"];then
+        echo "you selected Apple!"
+elif ["$FRUIT" == "ORANGE"];then
+        echo "you selected Orange"
+else
+        echo "you selected other Fruit!"
+fi
