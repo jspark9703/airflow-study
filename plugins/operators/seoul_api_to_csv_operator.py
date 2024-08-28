8d9118c455d0984a4f71523ea34861e285f006c4
JSPARK__\jspar <jspark9703@gmail.com>
2024-08-28 17:20:27 +0900

    asdf

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -51,7 +51,7 @@ class SeoulApiToCsvOperator(BaseOperator):
             request_url = f"{base_url}/{start_row}/{end_row}/{self.base_dt}"
         
         response = requests.get(request_url, headers)
-        print(response)
+        print(response.text)
         contents = json.loads(response.text)
         
         key_nm = list(contents.keys())[0]
