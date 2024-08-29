c1c71ff26e7156bf5c1e7f21aac8634894ab78b3
JSPARK__\jspar <jspark9703@gmail.com>
2024-08-29 20:19:35 +0900

    bulk load

--- dags/dag_python_with_postgres_hooks.py
+++ dags/dag_python_with_postgres_hooks.py
@@ -1,34 +1,22 @@
-import pendulum
 from airflow import DAG
+import pendulum
 from airflow.operators.python import PythonOperator
-
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
-    dag_id= "dag_python_with_postgres",
-    schedule=None,
-    start_date=pendulum.datetime(2024,8,27, tz="Asia/Seoul"),
-    catchup=False
-) as dag :
-    
-    def insrt_postgres(postgres_conn_id, tble_nm, file_cm, **kwargs):
-        from airflow.providers.postgres.hooks.postgres import PostgresHook
-        import psycopg2
-        from contextlib import closing 
+        dag_id='dags_python_with_postgres_hook_bulk_load',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule='0 7 * * *',
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
         postgres_hook = PostgresHook(postgres_conn_id)
-        postgres_hook.bulk_load(tble_nm, file_cm)
-        with closing(postgres_hook.get_conn()) as conn:
-            with closing(conn.cursor()) as cursor:
-                dag_id = kwargs.get("ti").dag_id
-                task_id = kwargs.get("ti").task_id
-                run_id = kwargs.get("ti").run_id
-                msg = "insrt 수행"
-                sql = "insert into py_opr_drct_insrt values (%s,%s,%s,%s);"
-                cursor.execute(sql,(dag_id, task_id, run_id,msg))
-                conn.commit()
-    insrt_postres = PythonOperator(
-        task_id = 'insrt_postgres',
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_kwargs= {"postgres_conn": "conn-db-postgres-custom", "tbl_nm":"Tb_bulk1", "file_nm": "/opt/airflow/files/tb_bulk/{{data_interval_end.in_timezone('Asia/Seoul')| ds_nodash}}/tb_bulk.csv"}
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
+                   'tbl_nm':'TbCorona19CountStatus_bulk1',
+                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
     )
-    
-    insrt_postgres
