b4a09c33f883e6952a6d12dd96c732334562df71
JSPARK__\jspar <jspark9703@gmail.com>
2024-09-01 17:46:19 +0900

    대규모 Rshiny 업뎃

--- dags/dag_python_with_custom_hook_bulk_load.py
+++ dags/dag_python_with_custom_hook_bulk_load.py
@@ -2,21 +2,37 @@ from airflow import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
 from hooks.custom_postgres_hook import CustomPostgresHook
+from airflow.utils.dates import days_ago
+
+# Define the start and end dates
+start_date = pendulum.datetime(2023, 4, 19, tz='Asia/Seoul')
+end_date = pendulum.datetime(2023, 5, 31, tz='Asia/Seoul')
 
 with DAG(
         dag_id='dags_python_with_custom_hook_bulk_load',
-        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
-        schedule='0 7 * * *',
+        start_date=start_date,
+        schedule=None,  # Since we'll manage the schedule manually
         catchup=False
 ) as dag:
+    
     def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
         custom_postgres_hook = CustomPostgresHook(postgres_conn_id=postgres_conn_id)
         custom_postgres_hook.bulk_load(table_name=tbl_nm, file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
 
-    insrt_postgres = PythonOperator(
-        task_id='insrt_postgres',
-        python_callable=insrt_postgres,
-        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
-                   'tbl_nm':'TbCorona19CountStatus_bulk2',
-                   'file_nm':'/opt/airflow/files/TbUseYearstatusView{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}.csv'}
-    )
+    # Create a task for each date in the range
+    current_date = start_date
+    while current_date <= end_date:
+        formatted_date = current_date.format('YYYYMMDD')
+        file_path = f'/opt/airflow/files/TbCorona19CountStatus/{formatted_date}/{formatted_date}.csv'
+        
+        insrt_postgres_task = PythonOperator(
+            task_id=f'insrt_postgres_{formatted_date}',
+            python_callable=insrt_postgres,
+            op_kwargs={
+                'postgres_conn_id': 'conn-db-postgres-custom',
+                'tbl_nm': 'TbCorona19CountStatus_bulk2',
+                'file_nm': file_path
+            }
+        )
+        
+        current_date = current_date.add(days=1)
